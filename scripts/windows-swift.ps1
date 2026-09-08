f7c4682fab34d7e8e6b70952982e98d0d9354d72
Pascal Matthiesen <me@pascal.sh>
2026-09-08 10:16:28 -0700

    fix: define NOMINMAX before windows.h on Windows CI
    minwindef.h min/max macros break std::min and
    std::numeric_limits::max in CNIOBoringSSL.

--- scripts/windows-swift.ps1
+++ scripts/windows-swift.ps1
@@ -28,6 +28,9 @@ $prefix = @"
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
 #endif
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <windows.h>
