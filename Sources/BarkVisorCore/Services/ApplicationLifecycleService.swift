8641e1379033b1beae8978dfc73ee4ff3e8107b8
Pascal Matthiesen <me@pascal.sh>
2026-09-25 22:15:04 +0000

    fix: split application lifecycle methods to satisfy lint

--- Sources/BarkVisorCore/Services/ApplicationLifecycleService.swift
+++ Sources/BarkVisorCore/Services/ApplicationLifecycleService.swift
@@ -481,7 +481,9 @@ public enum ApplicationLifecycleService {
         if let name = vm.composeProject, !name.isEmpty { return name }
         return ComposeRuntime.composeProjectName(id: vm.id)
     }
+}
 
+extension ApplicationLifecycleService {
     private static func syncProjectLocked(
         vm: inout VM,
         db: DatabasePool,
